73e248d952c022969bdfb76e7655173429783e03
Jason Stewart <jason00stewart@gmail.com>
2023-12-14 17:15:14 -0700

    Created Redistributer.hpp

---
+++ src/Redistributer.hpp
@@ -0,0 +1,65 @@
+/****************************************************************************
+ * Copyright (c) 2021, 2022 by the Beatnik author                           *
+ * All rights reserved.                                                     *
+ *                                                                          *
+ * This file is part of the Beatnik benchmark. Beatnik is                   *
+ * distributed under a BSD 3-clause license. For the licensing terms see    *
+ * the LICENSE file in the top-level directory.                             *
+ *                                                                          *
+ * SPDX-License-Identifier: BSD-3-Clause                                    *
+ ****************************************************************************/
+
+#ifndef BEATNIK_REDISTRIBUTER
+#define BEATNIK_REDISTRIBUTER
+
+#include <Cabana_Grid.hpp>
+
+#include <Kokkos_Core.hpp>
+
+#include <memory>
+
+#include <mpi.h>
+
+#include <limits>
+
+namespace Beatnik
+{
+//---------------------------------------------------------------------------//
+/*!
+  \class Redistributer
+  \brief Logically uniform Cartesian mesh.
+*/
+template <class ExecutionSpace, class MemorySpace>
+class Redistributer
+{
+  public:
+    using memory_space = MemorySpace;
+    using device_type = Kokkos::Device<ExecutionSpace, MemorySpace>;
+    using mesh_type = Cabana::Grid::UniformMesh<double, 2>;
+
+    // Construct a mesh.
+    Redistributer( const std::array<double, 6>& global_bounding_box,
+          const std::array<int, 2>& num_nodes,
+	      const std::array<bool, 2>& periodic,
+          const Cabana::Grid::BlockPartitioner<2>& partitioner,
+          const int min_halo_width, MPI_Comm comm )
+		  : _num_nodes( num_nodes )
+    {
+        MPI_Comm_rank( comm, &_rank );
+
+    }
+
+    
+
+  private:
+    std::array<double, 3> _low_point, _high_point;
+    std::shared_ptr<Cabana::Grid::LocalGrid<mesh_type>> _local_grid;
+    int _rank;
+	std::array<int, 2> _num_nodes;
+};
+
+//---------------------------------------------------------------------------//
+
+} // end namespace Beatnik
+
+#endif // end BEATNIK_REDISTRIBUTER
