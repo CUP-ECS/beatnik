d4ec3e3f5154cf18155a5ad9156a73038bba590b
Jason Stewart <jason00stewart@gmail.com>
2024-01-02 16:40:05 -0700

    Added functions for BR calcs

--- src/Migrator.hpp
+++ src/Migrator.hpp
@@ -188,6 +188,211 @@ class Migrator
         }
     }
 
+    void computeInterfaceVelocityNeighbors(int neighbor_radius)
+    {
+        /* Project the Birkhoff-Rott calculation between all pairs of points on the 
+         * interface, including accounting for any periodic boundary conditions.
+         * Right now we brute force all of the points with no tiling to improve
+         * memory access or optimizations to remove duplicate calculations. */
+        /* Figure out which directions we need to project the k/l point to
+         * for any periodic boundary conditions */
+
+        // int kstart, lstart, kend, lend;
+        // if (_bc.isPeriodicBoundary({0, 1})) {
+        //     kstart = -1; kend = 1;
+        // } else {
+        //     kstart = kend = 0;
+        // }
+        // if (_bc.isPeriodicBoundary({1, 1})) {
+        //     lstart = -1; lend = 1;
+        // } else {
+        //     lstart = lend = 0;
+        // }
+
+        /* Figure out how wide the bounding box is in each direction */
+        // auto low = _pm.mesh().boundingBoxMin();
+        // auto high = _pm.mesh().boundingBoxMax();;
+        // double width[3];
+        // for (int d = 0; d < 3; d++) {
+        //     width[d] = high[d] - low[d];
+        // }
+
+        double dx = _dx, dy = _dy, epsilon = _epsilon;
+
+        // Find neighbors using ArborX
+        //auto ids = Cabana::slice<3>(_particle_array);
+        auto positions = Cabana::slice<0>(_particle_array);
+        // for (int i = 0; i < positions.size(); i++) {
+        //     auto tp = _particle_array.getTuple( i );
+        //     printf("R%d: ID %d: %0.5lf %0.5lf %0.5lf\n", rank, Cabana::get<4>(tp),
+        //         Cabana::get<0>(tp, 0), Cabana::get<0>(tp, 1), Cabana::get<0>(tp, 2));
+        // }
+
+        std::size_t num_particles = positions.size();
+
+        auto neighbor_list = Cabana::Experimental::makeNeighborList<device_type>(
+        Cabana::FullNeighborTag{}, positions, 0, num_particles,
+            neighbor_radius);
+
+        using list_type = decltype(neighbor_list);
+        particle_array_type particle_array = _particle_array;
+        int rank = _rank;
+
+        Kokkos::parallel_for("compute_BR_with_neighbors", num_particles, KOKKOS_LAMBDA(int i) {
+
+            int num_neighbors = Cabana::NeighborList<list_type>::numNeighbor(neighbor_list, i);
+            double brsum[3] = {0.0, 0.0, 0.0};
+            auto particle = particle_array.getTuple(i);
+
+            if (Cabana::get<6>(particle) != rank) return;
+
+            // Compute BR forces from neighbors
+            int i_index = Cabana::get<4>(particle, 0);
+            int j_index = Cabana::get<4>(particle, 1);
+            //printf("Particle %d/%lu, num neighbors %d\n", i, num_particles, num_neighbors);
+
+            for (int j = 0; j < num_neighbors; j++) {
+                int neighbor_id = Cabana::NeighborList<list_type>::getNeighbor(neighbor_list, i, j);
+                auto neighbor_particle =  particle_array.getTuple(neighbor_id);
+                double weight = Cabana::get<3>(neighbor_particle);
+
+                // XXX Offset initializtion not correct for periodic boundaries
+                double offset[3] = {0.0, 0.0, 0.0}, br[3];
+                
+                /* Do the Birkhoff-Rott evaluation for this point */
+                Operators::BR_with_aosoa(br, particle, neighbor_particle, epsilon, dx, dy, weight, offset);
+                for (int d = 0; d < 3; d++) {
+                    brsum[d] += br[d];
+                }
+                // if (i == 20) {
+                //     printf("R%d: neighbor %d, weight: %0.13lf\n", rank, neighbor_id, weight);
+                // }
+            }
+            
+            // Update the AoSoA
+            for (int n = 0; n < 3; n++) {
+                Cabana::get<2>(particle, n) = brsum[n];
+            }
+            particle_array.setTuple(i, particle);
+            // if (i == 20) {
+            //     printf("R%d: Particle %d/%lu (%d, %d),br_sum: %0.13lf %0.13lf %0.13lf\n", rank, i, num_particles, i_index, j_index, brsum[0], brsum[1], brsum[2]);
+            // }
+        });
+
+        // Wait for all parallel tasks to complete
+        Kokkos::fence();
+
+        // for ( std::size_t i = 0; i < positions.size(); ++i )
+        // {
+        //     int num_neighbors = Cabana::NeighborList<list_type>::numNeighbor(neighbor_list, i);
+
+        //     //std::cout << "R" << rank << ": Particle " << i << " # neighbor = " << num_neighbors << std::endl;
+        //     // int num_n = Cabana::NeighborList<list_type>::numNeighbor( neighbor_list, i );
+        //     // for ( int j = 0; j < num_n; ++j )
+        //     //     std::cout << "    neighbor " << j << " = "
+        //     //             << Cabana::NeighborList<list_type>::getNeighbor(
+        //     //                     neighbor_list, i, j )
+        //     //             << std::endl;
+
+        //     // Compute BR force from neighbors
+            
+        //     double brsum[3] = {0.0, 0.0, 0.0};
+        //     auto particle = _particle_array.getTuple(i);
+
+        //     // Only calculate for particles owned by this rank, not ghosts
+        //     if (Cabana::get<6>(particle) != _rank) continue;
+
+        //     int i_index = Cabana::get<4>(particle, 0);
+        //     int j_index = Cabana::get<4>(particle, 1);
+        //     for (int j = 0; j < num_neighbors; j++) {
+        //         int neighbor_id = Cabana::NeighborList<list_type>::getNeighbor(neighbor_list, i, j);
+        //         auto neighbor_particle = _particle_array.getTuple(neighbor_id);
+
+        //         double weight = Cabana::get<3>(neighbor_particle);
+
+        //         // XXX Offset initializtion not correct for periodic boundaries
+        //         double offset[3] = {0.0, 0.0, 0.0}, br[3];
+                
+        //         /* Do the Birkhoff-Rott evaluation for this point */
+        //         Operators::BR_with_aosoa(br, particle, neighbor_particle, epsilon, dx, dy, weight, offset);
+        //         for (int d = 0; d < 3; d++) {
+        //             brsum[d] += br[d];
+        //         }
+        //     }
+        //     /* Add it its contribution to the integral */
+        //     for (int n = 0; n < 3; n++) {
+        //         Cabana::get<2>(particle, n) = brsum[n];
+        //     }
+
+        //     // Update the AoSoA
+        //     _particle_array.setTuple(i, particle);
+            
+
+        //     // if (i_index == 4 && j_index == 9) {
+        //     //     printf("xyz: %0.5lf %0.5lf %0.5lf\nomega: %0.5lf %0.5lf %0.5lf\nzdot: %0.13lf %0.13lf %0.13lf\nsimpson: %0.5lf, (%d, %d), id: %d\n", 
+        //     //         Cabana::get<0>(particle, 0), Cabana::get<0>(particle, 1), Cabana::get<0>(particle, 2),
+        //     //         Cabana::get<1>(particle, 0), Cabana::get<1>(particle, 1), Cabana::get<1>(particle, 2),
+        //     //         Cabana::get<2>(particle, 0), Cabana::get<2>(particle, 1), Cabana::get<2>(particle, 2),
+        //     //         Cabana::get<3>(particle), Cabana::get<4>(particle, 0), Cabana::get<4>(particle, 1), Cabana::get<5>(particle));
+        //     // }
+        // }
+    }
+
+    template <class PositionView>
+    void populate_zdot(PositionView zdot)
+    {
+        particle_array_type particle_array = _particle_array;
+        int rank = _rank;
+
+        // Parallel for loop using Kokkos
+        Kokkos::parallel_for("update_zdot", _array_size, KOKKOS_LAMBDA(int i) {
+            auto particle = particle_array.getTuple(i);
+            int particle_rank = Cabana::get<6>(particle);
+
+            if (particle_rank == rank) {
+                int i_index = Cabana::get<4>(particle, 0);
+                int j_index = Cabana::get<4>(particle, 1);
+
+                for (int n = 0; n < 3; n++) {
+                    Kokkos::atomic_add(&zdot(i_index, j_index, n), Cabana::get<2>(particle, n));
+                }
+            }
+        });
+
+        // Wait for all parallel tasks to complete
+        Kokkos::fence();
+
+
+        //if (_rank == 0) {
+            //printf("R%d: ****START**** size = %d, array_size = %d\n", _rank, _particle_array.size(), _array_size);
+        //}
+        // for (int i = 0; i < _array_size; i++)
+        // {
+        //     if (_rank == 0) {
+        //         //printf("R%d: i = %d\n", _rank, i);
+        //     }
+        //     auto particle = _particle_array.getTuple(i);
+
+        //     // Check that the particle is owned by our rank, and thus in zdot
+        //     int particle_rank = Cabana::get<6>(particle);
+        //     if (_rank == 0) {
+        //         //printf("R%d: i%d: p-rank: %d\n", _rank, i, particle_rank);
+        //     }
+        //     if (particle_rank == _rank)
+        //     {
+        //         int i_index = Cabana::get<4>(particle, 0);
+        //         int j_index = Cabana::get<4>(particle, 1);
+        //         //if (_rank == 0) {
+        //            // printf("R%d: i%d: p-rank: %d, i-index: %d, j-index: %d\n", _rank, i, particle_rank, i_index, j_index);
+        //         //}
+        //         for (int n = 0; n < 3; n++) 
+        //         {
+        //             zdot(i_index, j_index, n) = Cabana::get<2>(particle, n);
+        //         }
+        //     }
+        // }
+    }
+
   //private:
     particle_array_type _particle_array;
     const spatial_mesh_type &_spm;
