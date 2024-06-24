5ac3bb58822fc97877c0bdd5ce08dbf181a2c2b1
Jason Stewart <jason00stewart@gmail.com>
2024-06-24 12:21:00 -0600

    quicksave

--- tests/tstExactBRSolver.hpp
+++ tests/tstExactBRSolver.hpp
@@ -23,6 +23,20 @@ class TestExactBRSolver : public testing::Test
     /* Init the classes required for the BR solver to work */
     void SetUp() override
     {
+
+        // Create the mesh
+        Cabana::Grid::DimBlockPartitioner<2> partitioner;
+
+
+        auto node_triple_layout =
+            Cabana::Grid::createArrayLayout( pm.mesh().localGrid(), 3, Cabana::Grid::Node() );
+        auto node_pair_layout =
+
+            Cabana::Grid::createArrayLayout( pm.mesh().localGrid(), 2, Cabana::Grid::Node() );
+        _zdot = Cabana::Grid::createArray<double, mem_space>("velocity", 
+                                                       node_triple_layout);
+        _wdot = Cabana::Grid::createArray<double, mem_space>("vorticity derivative",
+                                                       node_pair_layout);
         /* ExactBRSolver( const pm_type & pm, const BoundaryCondition &bc,
                    const double epsilon, const double dx, const double dy)*/
         
@@ -30,15 +44,13 @@ class TestExactBRSolver : public testing::Test
 
 
     // Variables for needed testing ExactBRSolver
-    const ProblemManager & _pm;
-    const BoundaryCondition & _bc;
-    double _epsilon, _dx, _dy;
-    MPI_Comm _comm;
+    const 
 }
 
-TEST_F(TestExactBRSolver, sampleTest)
-{
-  ASSERT_EQ(6, 6);
+TEST_F(TestExactBRSolver, testComputeInterfaceVelocityPeriodicBC)
+{ 
+
+    ASSERT_EQ(6, 6);
 }
 
 
