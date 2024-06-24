a280df9cdd72e7f9d36dd75a99aac74c989ced8f
Jason Stewart <jason00stewart@gmail.com>
2024-06-24 15:00:52 -0600

    in progress

--- tests/tstExactBRSolver.hpp
+++ tests/tstExactBRSolver.hpp
@@ -8,48 +8,49 @@
 #include <Kokkos_Core.hpp>
 
 #include <Solver.hpp>
+#include <examples/rocketrig>
 
 #include <mpi.h>
 
 #include "tstExactBRSolverCorrect.hpp"
 #include "tstDriver.hpp"
 
-
 class TestExactBRSolver : public testing::Test
 {
   protected:
     
-
-    /* Init the classes required for the BR solver to work */
     void SetUp() override
     {
-
-        // Create the mesh
-        Cabana::Grid::DimBlockPartitioner<2> partitioner;
-
-
-        auto node_triple_layout =
-            Cabana::Grid::createArrayLayout( pm.mesh().localGrid(), 3, Cabana::Grid::Node() );
-        auto node_pair_layout =
-
-            Cabana::Grid::createArrayLayout( pm.mesh().localGrid(), 2, Cabana::Grid::Node() );
-        _zdot = Cabana::Grid::createArray<double, mem_space>("velocity", 
-                                                       node_triple_layout);
-        _wdot = Cabana::Grid::createArray<double, mem_space>("vorticity derivative",
-                                                       node_pair_layout);
-        /* ExactBRSolver( const pm_type & pm, const BoundaryCondition &bc,
-                   const double epsilon, const double dx, const double dy)*/
+       
         
     }
 
-
-    // Variables for needed testing ExactBRSolver
-    const 
 }
 
 TEST_F(TestExactBRSolver, testComputeInterfaceVelocityPeriodicBC)
 { 
+    /* Tests rocketrig with the default initial conditions 
+     * and using the ExactBRSolver with the following modifications:
+     *  - 4 processes
+     *  - 32x32 mesh
+     *  - Checks after timestep 10 (-t 10)
+     **/
 
+    // Problem setup
+    Cabana::Grid::DimBlockPartitioner<2> partitioner;
+
+    std::array<double, 6> global_bounding_box = {-1.0, -1.0, -1.0, 1.0, 1.0, 1.0};
+    std::array<int, 2> num_nodes = {128, 128};
+    double gravity = 25.0;
+    double atwood = 0.5;
+
+     solver = Beatnik::createSolver(
+            "serial", MPI_COMM_WORLD,
+            cl.global_bounding_box, cl.num_nodes,
+            partitioner, cl.atwood, cl.gravity, initializer,
+            bc, Beatnik::Order::High(), cl.mu, cl.eps, cl.delta_t );
+
+    EXPECT_DOUBLE_EQ(1.0, 1.000000000001);
     ASSERT_EQ(6, 6);
 }
 
