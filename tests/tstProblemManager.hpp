40667ad1561258702a7859fef198a238dc799d9c
Jason Stewart <jason00stewart@gmail.com>
2024-07-03 15:28:10 -0600

    In-progress attempt to make test work

--- tests/tstProblemManager.hpp
+++ tests/tstProblemManager.hpp
@@ -2,7 +2,7 @@
 #define _TSTPROBLEMMANGER_HPP_
 
 #include <Cabana_Core.hpp>
-#include <Cajita.hpp>
+#include <Cabana_Grid.hpp>
 #include <Kokkos_Core.hpp>
 #include <ProblemManager.hpp>
 
@@ -10,12 +10,15 @@
 
 #include "tstMesh.hpp"
 
+namespace BeatnikTest
+{
+
 template <std::size_t Dim>
 class NullInitFunctor
 {
   public:
     KOKKOS_INLINE_FUNCTION
-    bool operator()( Cajita::Node, Beatnik::Field::Position,
+    bool operator()( Cabana::Grid::Node, Beatnik::Field::Position,
                      [[maybe_unused]] const int index[Dim],
                      [[maybe_unused]] const double x[Dim],
                      [[maybe_unused]] double& z1, 
@@ -26,7 +29,7 @@ class NullInitFunctor
     };
 
     KOKKOS_INLINE_FUNCTION
-    bool operator()( Cajita::Node, Beatnik::Field::Vorticity,
+    bool operator()( Cabana::Grid::Node, Beatnik::Field::Vorticity,
                      [[maybe_unused]] const int index[Dim],
                      [[maybe_unused]] const double x[Dim],
                      [[maybe_unused]] double& w1,
@@ -46,22 +49,47 @@ class ProblemManagerTest : public MeshTest<T>
     using ExecutionSpace = typename T::ExecutionSpace;
     using MemorySpace = typename T::MemorySpace;
 
-  public:
+  protected:
     NullInitFunctor<2> createFunctor_;
-    std::shared_ptr<pm_type> testPM_;
+    std::shared_ptr<pm_type> testPM_Periodic_;
+    std::shared_ptr<pm_type> testPM_NonPeriodic_;
 
-    virtual void SetUp() override
+    void SetUp() override
     {
         MeshTest<T>::SetUp();
-        this->testPM_ =
-            std::make_shared<pm_type>( *this->testMesh_, createFunctor_ );
+        this->testPM_Periodic_ =
+            std::make_shared<pm_type>( *this->testMeshPeriodic_, BoundaryConditionTest::bc_periodic_, createFunctor_ );
+        this->testPM_NonPeriodic_ =
+            std::make_shared<pm_type>( *this->testMeshNonPeriodic_, BoundaryConditionTest::bc_non_periodic, createFunctor_ );
     }
 
-    virtual void TearDown() override
+    void TearDown() override
     {
-        this->testPM_ = NULL;
+        this->testMeshPeriodic_ = NULL;
+        this->testMeshNonPeriodic_ = NULL;
         MeshTest<T>::TearDown();
     }
+
+  public:
+    template<class ProblemManager>
+    void initializePositions(ProblemManager &pm)
+    {
+        auto mesh = pm.mesh();
+        auto rank = mesh.rank();
+        auto z = pm.get( Cabana::Grid::Node(), Beatnik::Field::Position() );
+        // auto zspace = mesh->localGrid()->indexSpace( Cabana::Grid::Own(), Cabana::Grid::Node(),
+        //                                             Cabana::Grid::Local() );
+        // Kokkos::parallel_for(
+        //     "InitializePositions",
+        //     createExecutionPolicy( zspace, ExecutionSpace ),
+        //     KOKKOS_LAMBDA( const int i, const int j ) {
+        //         for (int d = 0; d < 3; d++)
+        //             z( i, j, d ) = rank * 1000 + i * 100 + j * 10 + d;
+        //     } );
+    }
+
 };
+
+} // end namespace BeatnikTest
 
 #endif // _TSTPROBLEMMANAGER_HPP_
