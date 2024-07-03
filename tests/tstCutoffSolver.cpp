a3d6981dd6b9228266a80afffb0a3699b4be6dc6
Jason Stewart <jason00stewart@gmail.com>
2024-07-03 17:49:20 -0600

    quicksave

--- tests/tstCutoffSolver.cpp
+++ tests/tstCutoffSolver.cpp
@@ -13,7 +13,7 @@ namespace BeatnikTest
 
 TYPED_TEST_SUITE(CutoffSolverTest, DeviceTypes);
 
-TYPED_TEST(CutoffSolverTest, testFreeBoundary)
+TYPED_TEST(CutoffSolverTest, testNeighboringRanks)
 { 
     int rank, comm_size;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
@@ -21,14 +21,24 @@ TYPED_TEST(CutoffSolverTest, testFreeBoundary)
     auto boundary_topology = this->br_->get_spatial_mesh()->getBoundaryInfo();
     int local_location[3] = {boundary_topology(rank, 1), boundary_topology(rank, 2), boundary_topology(rank, 3)};
     int max_location[3] = {boundary_topology(comm_size, 1), boundary_topology(comm_size, 2), boundary_topology(comm_size, 3)};
-    for (int i = 0; i < comm_size; i++)
-    {
-        int remote_location[3] = {boundary_topology(i, 1), boundary_topology(i, 2), boundary_topology(i, 3)};
-        bool result = this->br_->isValidRank(remote_location, local_location, max_location);
-        printf("R%d, valid: %d\n", rank, result);
-    }
+    int remote_location[3] = {-1, -1, -1};
+    int correct = this->areNeighbors(remote_location, local_location, max_location);
+    printf("R%d, %d, %d, %d, is_n: %d\n", rank, local_location[0], local_location[1], local_location[2], correct);
+
+    // if (rank == 0)
+    // {
+    //     printf("ll: %d, %d, %d\n\n", local_location[0], local_location[1], local_location[2]);
+    //     for (int i = 0; i < comm_size; i++)
+    //     {
+    //         int remote_location[3] = {boundary_topology(i, 1), boundary_topology(i, 2), boundary_topology(i, 3)};
+    //         int result = this->br_->isValidRank(remote_location, local_location, max_location);
+    //         int correct = this->areNeighbors(remote_location, local_location, max_location);
+    //         printf("Neighbor: R%d, %d, %d, %d, is_n: %d\n", i, remote_location[0], remote_location[1], remote_location[2], correct);
+    //         //ASSERT_EQ(result, correct); 
+    //     }
+    // }
     
-    ASSERT_EQ(0, 0);    
+       
     // auto z = this->position_np_->view();
 
     // this->populateArray(z);
--- tests/tstCutoffSolver.hpp
+++ tests/tstCutoffSolver.hpp
@@ -94,45 +94,17 @@ class CutoffSolverTest : public MeshTest<T>
     }
 
   public:
-    template <class View>
-    void populateArray(View z)
+    int areNeighbors(const int remote_location[3], const int local_location[3], const int num_procs[3])
     {
-        /*****************
-         *  Getting execution range from MeshTest because the compiler does like the following:
-        auto own_nodes = local_grid_->indexSpace(Cabana::Grid::Own(), Cabana::Grid::Node(),
-                                                 Cabana::Grid::Local());
+        // Handle corners differently
+        if ((local_location[0] == 0 && local_location[1] == 0) ||
+                    (local_location[0] == num_procs[0]-1 &&
+                     local_location[1] == num_procs[1]-1)) // Top left and bottom right corners
+        {
+            //int n1 = 
+        }
 
-        auto policy = Cabana::Grid::createExecutionPolicy( own_nodes );
-        **************/
-        int range = MeshTest<T>::boxNodes_;
-        int min = MeshTest<T>::haloWidth_;
-        using range_policy = Kokkos::MDRangePolicy<Kokkos::Rank<2>, ExecutionSpace>;
-        range_policy policy({min, min}, {min+range, min+range});
-        double dx = 0.3, dy = 0.4;
-        Kokkos::parallel_for("Initialize Cells", policy,
-            KOKKOS_LAMBDA( const int i, const int j ) {
-                z(i, j, 0) = -1+dx*i + -1+dy*j;
-            });
-    }
-
-    template <class View>
-    void testFreeBC(View z)
-    {
-        int range = MeshTest<T>::boxNodes_;
-        int min = MeshTest<T>::haloWidth_;
-        using range_policy = Kokkos::MDRangePolicy<Kokkos::Rank<2>, ExecutionSpace>;
-        range_policy policy({0, 0}, {min+range+min, min+range+min});
-        double dx = 0.3, dy = 0.4;
-        Kokkos::parallel_for("Initialize Cells", policy,
-            KOKKOS_LAMBDA( const int i, const int j ) {
-                double correct_value = -1+dx*i + -1+dy*j;
-                /* Using EXPECT_NEAR because of floating-point imprecision
-                 * between doubles inside and out of a Kokkos view.
-                 * XXX - Fix calling the __host__ function from a __host__ __device__ function
-                 * warning caused by using EXPECT_NEAR here.
-                 */
-                EXPECT_NEAR(correct_value, z(i, j, 0), 0.000000000001);
-            });
+    return 0;
     }
 };
 
