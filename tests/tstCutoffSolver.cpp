7f92bc1b67f136dd265b8f0bb3a85ca3bd87ee74
Jason Stewart <jason00stewart@gmail.com>
2024-07-06 14:03:25 -0600

    Working on periodic halo test

--- tests/tstCutoffSolver.cpp
+++ tests/tstCutoffSolver.cpp
@@ -27,40 +27,19 @@ TYPED_TEST(CutoffSolverTest, testIsOnBoundary)
     EXPECT_EQ(result, correct_result) << "Rank " << rank << " incorrect.";
 }
 
-TYPED_TEST(CutoffSolverTest, testAreNeighbors)
+/* Tests whether points are haloed correctly in 3D space
+ * across x/y boundaries. Doesn't perform an explicit check, 
+ * but rather makes sure that at least one coordinate changed
+ * and that the adjusted position is no more than
+ * bounding box limit + cutoff distance outside of the mesh.
+ */
+TYPED_TEST(CutoffSolverTest, testPeriodicHalo)
 { 
     int rank, comm_size;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &comm_size);
     auto boundary_topology = this->p_br_cutoff_->get_spatial_mesh()->getBoundaryInfo();
-    int local_location[3] = {boundary_topology(rank, 1), boundary_topology(rank, 2), boundary_topology(rank, 3)};
-    int max_location[3] = {boundary_topology(comm_size, 1), boundary_topology(comm_size, 2), boundary_topology(comm_size, 3)};
-    //printf("R%d, %d, %d, %d, is_n: %d\n", rank, local_location[0], local_location[1], local_location[2], correct);
-
-    // Each rank will test if all any ranks share a periodic boundary in 3D x/y space
-    // Defined as one of its 26 neighboring ranks in 3D space.
-    int is_neighbor[26];
-    if (rank == 0)
-    {
-        this->getNeighbors(rank, boundary_topology, is_neighbor);
-    // for (int other_rank = 0; other_rank < comm_size; other_rank++)
-    // {
-    //     int remote_location[3] = {boundary_topology(other_rank, 1), boundary_topology(other_rank, 2), boundary_topology(other_rank, 3)};
-    //     int correct_result = this->getNeighbors();
-    // }
-    }
-    // if (rank == 0)
-    // {
-    //     printf("ll: %d, %d, %d\n\n", local_location[0], local_location[1], local_location[2]);
-    //     for (int i = 0; i < comm_size; i++)
-    //     {
-    //         int remote_location[3] = {boundary_topology(i, 1), boundary_topology(i, 2), boundary_topology(i, 3)};
-    //         int result = this->br_->isValidRank(remote_location, local_location, max_location);
-    //         int correct = this->areNeighbors(remote_location, local_location, max_location);
-    //         printf("Neighbor: R%d, %d, %d, %d, is_n: %d\n", i, remote_location[0], remote_location[1], remote_location[2], correct);
-    //         //ASSERT_EQ(result, correct); 
-    //     }
-    // }
+    
 }
 
 } // end namespace BeatnikTest
